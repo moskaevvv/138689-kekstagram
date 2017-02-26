d31a4bbb154be0682b919f5038c1d05cb5439f4e
Moskaevvv <moskaevvv@mail.ru>
2017-02-26 14:11:24 +0300

    Разобрался с коллбэками в initialize-filters.js, залить это в module6 не смог

--- js/form.js
+++ js/form.js
@@ -56,18 +56,17 @@ uploadFormCancel.addEventListener('keydown', overlayClosePress);
 
 
 
-
-
 //2. Применения фильтров
 
 
 (function() {
 
-        window.filterControlPanel = document.querySelector('.upload-filter-controls');
-    window.filterControls = filterControlPanel.querySelectorAll('input');
-    var filterControlLabels = filterControlPanel.querySelectorAll('.upload-filter-label');
+    var filterControlPanel = document.querySelector('.upload-filter-controls');
+    var filterControls = filterControlPanel.querySelectorAll('input');
     var filterImagePreview = document.querySelector('.filter-image-preview');
+    var filterID;
     
+    //callback
     var filterSwitcher = function(filterID) {
         filterImagePreview.classList.remove('filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
         if (filterID.substr(7) !== 'filter-none') {
@@ -75,116 +74,14 @@ uploadFormCancel.addEventListener('keydown', overlayClosePress);
         };
     };
     
-    var clickHandler = function(evt) {
-        var clickedElement;
-        clickedElement = evt.currentTarget;
-        filterID = clickedElement.id;
-    };
+    //2.1 Функция
+    window.initializeFilters.mouseMethod(filterControls, filterSwitcher);
+
+    //2.2 +ARIA
     
-    
-//2.1 Функция
+    var filterControlLabels = filterControlPanel.querySelectorAll('.upload-filter-label');
+    window.initializeFilters.enterMethod(filterControlLabels, filterControls, filterSwitcher);
 
-for (var i = 0; i < filterControls.length; i++) {
-    var filterID;
-    filterControls[i].addEventListener('click', clickHandler); 
-    filterControls[i].addEventListener('click', function() {
-        filterSwitcher(filterID);                               
-    });
-};
-
-    
-    /*
-//2.2 +ARIA
-
-var labelNumber;
-var filterControlLabels = filterControlPanel.querySelectorAll('.upload-filter-label');
-
-var unCheckRadios = function() {
-    for (var i = 0; i < filterControlLabels.length; i++) {
-        filterControlLabels[i].setAttribute('aria-checked', 'false');
-        filterControls[i].removeAttribute('checked');
-    };
-};
-
-var checkRadios = function(labelNumber) {
-    filterControlLabels[labelNumber].setAttribute('aria-checked', 'true');
-    filterControls[labelNumber].setAttribute('checked', 'true');
-};
-
-var noFilters = function(evt) {
-    labelNumber = 0;
-    if (evt.keyCode === ENTER_KEY_CODE) {
-        var filterID = filterControls[labelNumber].id;
-        var filterName = filterID.substr(7);
-        initializeFilters(filterName);
-        unCheckRadios();
-        checkRadios(labelNumber);
-    };
-};
-
-var filterChromeOn = function(evt) {
-    labelNumber = 1;
-    if (evt.keyCode === ENTER_KEY_CODE) {
-        var filterID = filterControls[labelNumber].id;
-        var filterName = filterID.substr(7);
-        initializeFilters(filterName);
-        unCheckRadios();
-        checkRadios(labelNumber);
-    };
-};
-
-var filterSepiaOn = function(evt) {
-    labelNumber = 2;
-    if (evt.keyCode === ENTER_KEY_CODE) {
-        var filterID = filterControls[labelNumber].id;
-        var filterName = filterID.substr(7);
-        initializeFilters(filterName);
-        unCheckRadios();
-        checkRadios(labelNumber);
-    };
-};
-
-var filterMarvinOn = function(evt) {
-    labelNumber = 3;
-    if (evt.keyCode === ENTER_KEY_CODE) {
-        var filterID = filterControls[labelNumber].id;
-        var filterName = filterID.substr(7);
-        initializeFilters(filterName);
-        unCheckRadios();
-        checkRadios(labelNumber);
-    };
-};
-
-var filterPhobosOn = function(evt) {
-    labelNumber = 4;
-    if (evt.keyCode === ENTER_KEY_CODE) {
-        var filterID = filterControls[labelNumber].id;
-        var filterName = filterID.substr(7);
-        initializeFilters(filterName);
-        unCheckRadios();
-        checkRadios(labelNumber);
-    };
-};
-
-var filterHeatOn = function(evt) {
-    labelNumber = 5;
-    if (evt.keyCode === ENTER_KEY_CODE) {
-        var filterID = filterControls[labelNumber].id;
-        var filterName = filterID.substr(7);
-        initializeFilters(filterName);
-        unCheckRadios();
-        checkRadios(labelNumber);
-    };
-};
-
-filterControlLabels[0].addEventListener('keydown', noFilters);
-filterControlLabels[1].addEventListener('keydown', filterChromeOn);
-filterControlLabels[2].addEventListener('keydown', filterSepiaOn);
-filterControlLabels[3].addEventListener('keydown', filterMarvinOn);
-filterControlLabels[4].addEventListener('keydown', filterPhobosOn);
-filterControlLabels[5].addEventListener('keydown', filterHeatOn);
-    
-    */
     
 })();
 
--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -2,26 +2,71 @@
 
 window.initializeFilters = (function() {
     
-var filterImagePreview = document.querySelector('.filter-image-preview');
-
-var filterAdd;
-var filtrationOn;
-
-return function(thisFilter) {
     
-    filterAdd = function() {
-        if (thisFilter !== 'filter-none') {
-            filterImagePreview.classList.add(thisFilter);
-        }
+    var filterID;
+    var filterControls;
+    var filterControlLabels;
+    
+    
+    return {
+        
+        mouseMethod: function(filterControls, filterSwitcher) {
+        
+        var clickHandlerAndCallback = function(evt, filterSwitcher) {
+            
+            var clickedElement;
+            clickedElement = evt.currentTarget;
+            filterID = clickedElement.id;
+        
+            if (typeof filterSwitcher === 'function') {
+                filterSwitcher(filterID);
+            };
+        };
+        
+        for (var i = 0; i < filterControls.length; i++) {
+            filterControls[i].addEventListener('click', function() {
+                clickHandlerAndCallback(event, filterSwitcher);                            
+            });
+        };
+    },
+
+        enterMethod: function(filterControlLabels, filterControls, filterSwitcher) {
+        
+            var unCheckRadios = function() {
+                for (var i = 0; i < filterControlLabels.length; i++) {
+                    filterControlLabels[i].setAttribute('aria-checked', 'false');
+                    filterControls[i].removeAttribute('checked');
+                };
+            };
+            
+            var checkRadios = function(i) {
+                filterControlLabels[i].setAttribute('aria-checked', 'true');
+                filterControls[i].setAttribute('checked', 'true');
+            };
+            
+            var actions = function(i, evt) {
+                if (evt.keyCode === ENTER_KEY_CODE) {
+                    filterID = filterControls[i].id;
+                    unCheckRadios();
+                    checkRadios(i);
+                };
+                if (typeof filterSwitcher === 'function') {
+                    filterSwitcher(filterID);
+                };    
+            };
+            
+            var enterAction = function(i) {
+                filterControlLabels[i].addEventListener('keydown', function() {
+                    actions(i, event);
+                });
+            };
+            
+            for (var i = 0; i < filterControlLabels.length; i++) {
+                enterAction(i);
+            };
+        },
+    
     };
+        
 
-    var cleanAllFilters = function(filterAdd) {
-    filterImagePreview.classList.remove('filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
-    };
-    
-    cleanAllFilters();
-     filterAdd();
-    
-};
-
-})();
+})()
