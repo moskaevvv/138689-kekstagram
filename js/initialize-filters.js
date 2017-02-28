97bae3fe939e9d243bbc3029fbaccb0bb83fbb2c
Moskaevvv <moskaevvv@mail.ru>
2017-02-28 21:07:40 +0300

    исправил ошибки ESlint

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,74 +1,74 @@
 'use strict';
 
-window.initializeFilters = (function() {
-    
-    
-    var filterID;
-    var filterControls;
-    var filterControlLabels;
-    
-    
-    return {
-        
-        mouseMethod: function(filterControls, filterSwitcher) {
-        
-        var clickHandlerAndCallback = function(evt, filterSwitcher) {
-            
-            var clickedElement;
-            clickedElement = evt.currentTarget;
-            filterID = clickedElement.id;
-        
-            if (typeof filterSwitcher === 'function') {
-                filterSwitcher(filterID);
-            };
+window.initializeFilters = (function () {
+
+
+  var filterID;
+  var filterControls;
+  var filterControlLabels;
+
+
+  return {
+
+    mouseMethod: function (filterControls, filterSwitcher) {
+
+      var clickHandlerAndCallback = function (evt, filterSwitcher) {
+
+        var clickedElement;
+        clickedElement = evt.currentTarget;
+        filterID = clickedElement.id;
+
+        if (typeof filterSwitcher === 'function') {
+          filterSwitcher(filterID);
+        }
+      };
+
+      for (var i = 0; i < filterControls.length; i++) {
+        filterControls[i].onclick = function (event) {
+          event = event || window.event;
+          clickHandlerAndCallback(event, filterSwitcher);
         };
-        
-        for (var i = 0; i < filterControls.length; i++) {
-            filterControls[i].onclick = function(event) {
-                event = event || window.event;
-                clickHandlerAndCallback(event, filterSwitcher);                   
-            };
-        };
+      }
     },
 
-        enterMethod: function(filterControlLabels, filterControls, filterSwitcher) {
-        
-            var unCheckRadios = function() {
-                for (var i = 0; i < filterControlLabels.length; i++) {
-                    filterControlLabels[i].setAttribute('aria-checked', 'false');
-                    filterControls[i].removeAttribute('checked');
-                };
-            };
-            
-            var checkRadios = function(i) {
-                filterControlLabels[i].setAttribute('aria-checked', 'true');
-                filterControls[i].setAttribute('checked', 'true');
-            };
-            
-            var actions = function(i, evt) {
-                if (evt.keyCode === ENTER_KEY_CODE) {
-                    filterID = filterControls[i].id;
-                    unCheckRadios();
-                    checkRadios(i);
-                };
-                if (typeof filterSwitcher === 'function') {
-                    filterSwitcher(filterID);
-                };    
-            };
-            
-            var enterAction = function(i) {
-                filterControlLabels[i].onkeypress = function(event) {
-                    event = event || window.event;
-                    actions(i, event);
-                };
-            };
-            
-            for (var i = 0; i < filterControlLabels.length; i++) {
-                enterAction(i);
-            };
-        },
-    
-    };
-        
+    enterMethod: function (filterControlLabels, filterControls, filterSwitcher) {
 
-})()
+      var unCheckRadios = function () {
+        for (var i = 0; i < filterControlLabels.length; i++) {
+          filterControlLabels[i].setAttribute('aria-checked', 'false');
+          filterControls[i].removeAttribute('checked');
+        }
+      };
+
+      var checkRadios = function (i) {
+        filterControlLabels[i].setAttribute('aria-checked', 'true');
+        filterControls[i].setAttribute('checked', 'true');
+      };
+
+      var actions = function (i, evt) {
+        if (evt.keyCode === ENTER_KEY_CODE) {
+          filterID = filterControls[i].id;
+          unCheckRadios();
+          checkRadios(i);
+        }
+        if (typeof filterSwitcher === 'function') {
+          filterSwitcher(filterID);
+        }
+      };
+
+      var enterAction = function (i) {
+        filterControlLabels[i].onkeypress = function (event) {
+          event = event || window.event;
+          actions(i, event);
+        };
+      };
+
+      for (var i = 0; i < filterControlLabels.length; i++) {
+        enterAction(i);
+      }
+    },
+
+  };
+
+
+})();
--- js/pictures.js
+++ js/pictures.js
@@ -1,52 +1,52 @@
 'use strict';
 
-(function() {
-    
-    var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
-    var pictures = [];
-    
-    var allPictures = document.querySelector('.pictures');
-    var templateElement = document.querySelector('#picture-template');
-    var elementToClone = templateElement.content.querySelector('.picture');
-    
-    var showAllPictures = function(array) {
+(function () {
 
-          array.forEach(function(picture) {
-              var newElement = elementToClone.cloneNode(true);
+  var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
+  var pictures = [];
 
-               var openGallery = function (event) {
-                event.preventDefault();
-                window.showGallery(picture);
-              };
+  var allPictures = document.querySelector('.pictures');
+  var templateElement = document.querySelector('#picture-template');
+  var elementToClone = templateElement.content.querySelector('.picture');
 
-              var keyboardOpenGallery = function (event) {
-                  if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
-                      openGallery(event);
-                  }
-              };
+  var showAllPictures = function (array) {
 
-              newElement.querySelector('img').src = picture.url;
-              newElement.querySelector('.picture-likes').textContent = picture.likes;
-              newElement.querySelector('.picture-comments').textContent = picture.comments.length;
-              newElement.addEventListener('click', openGallery);
-              newElement.addEventListener('keydown', keyboardOpenGallery);
+    array.forEach(function (picture) {
+      var newElement = elementToClone.cloneNode(true);
 
-            allPictures.appendChild(newElement);
-          });
+      var openGallery = function (event) {
+        event.preventDefault();
+        window.showGallery(picture);
+      };
 
-    };
-    
-    var pictureFiltersRow = document.querySelector('.filters'); 
-    var showPicturesFilter = function () {
-        pictureFiltersRow.classList.remove('hidden');
-    };
-    
-    var onLoad = function(event) {
-        pictures = event.target.response;
-        showPicturesFilter();
-        showAllPictures(pictures);
-    };
-    
-    window.load(DATA_URL, onLoad);
-    
+      var keyboardOpenGallery = function (event) {
+        if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
+          openGallery(event);
+        }
+      };
+
+      newElement.querySelector('img').src = picture.url;
+      newElement.querySelector('.picture-likes').textContent = picture.likes;
+      newElement.querySelector('.picture-comments').textContent = picture.comments.length;
+      newElement.addEventListener('click', openGallery);
+      newElement.addEventListener('keydown', keyboardOpenGallery);
+
+      allPictures.appendChild(newElement);
+    });
+
+  };
+
+  var pictureFiltersRow = document.querySelector('.filters');
+  var showPicturesFilter = function () {
+    pictureFiltersRow.classList.remove('hidden');
+  };
+
+  var onLoad = function (event) {
+    pictures = event.target.response;
+    showPicturesFilter();
+    showAllPictures(pictures);
+  };
+
+  window.load(DATA_URL, onLoad);
+
 })();
