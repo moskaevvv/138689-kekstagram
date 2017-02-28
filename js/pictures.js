a66c3f5635d00a29375eb0bdc11dc8d26f39b8d9
Moskaevvv <moskaevvv@mail.ru>
2017-02-28 18:18:16 +0300

    Начал фильтрацию изображений в выдаче. Нужен переход в новую ветку

--- js/pictures.js
+++ js/pictures.js
@@ -8,42 +8,45 @@
     var allPictures = document.querySelector('.pictures');
     var templateElement = document.querySelector('#picture-template');
     var elementToClone = templateElement.content.querySelector('.picture');
+    
+    var showAllPictures = function(array) {
 
-    
-  var showAllPictures = function(array) {
-      
-      array.forEach(function(picture) {
-          var newElement = elementToClone.cloneNode(true);
-          
-           var openGallery = function (event) {
-            event.preventDefault();
-            window.showGallery(picture);
-          };
-          
-          var keyboardOpenGallery = function (event) {
-              if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
-                  openGallery(event);
-              }
-          };
+          array.forEach(function(picture) {
+              var newElement = elementToClone.cloneNode(true);
 
-          newElement.querySelector('img').src = picture.url;
-          newElement.querySelector('.picture-likes').textContent = picture.likes;
-          newElement.querySelector('.picture-comments').textContent = picture.comments.length;
-          newElement.addEventListener('click', openGallery);
-          newElement.addEventListener('keydown', keyboardOpenGallery);
-          
-        allPictures.appendChild(newElement);
-      });
-  
-  };
-    
-    
-  var onLoad = function(event) {
-    pictures = event.target.response;
-    showAllPictures(pictures);
-  };
-    
+               var openGallery = function (event) {
+                event.preventDefault();
+                window.showGallery(picture);
+              };
 
-  window.load(DATA_URL, onLoad);
+              var keyboardOpenGallery = function (event) {
+                  if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
+                      openGallery(event);
+                  }
+              };
+
+              newElement.querySelector('img').src = picture.url;
+              newElement.querySelector('.picture-likes').textContent = picture.likes;
+              newElement.querySelector('.picture-comments').textContent = picture.comments.length;
+              newElement.addEventListener('click', openGallery);
+              newElement.addEventListener('keydown', keyboardOpenGallery);
+
+            allPictures.appendChild(newElement);
+          });
+
+    };
+    
+    var pictureFiltersRow = document.querySelector('.filters'); 
+    var showPicturesFilter = function () {
+        pictureFiltersRow.classList.remove('hidden');
+    };
+    
+    var onLoad = function(event) {
+        pictures = event.target.response;
+        showPicturesFilter();
+        showAllPictures(pictures);
+    };
+    
+    window.load(DATA_URL, onLoad);
     
 })();
