b14268e7524f4cbbd05c50018f7fbc775fd10673
Moskaevvv <moskaevvv@mail.ru>
2017-02-28 17:13:58 +0300

    коллбэк для изменения масштаба

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,54 +1,46 @@
 'use strict';
 
-window.initializeScale = (function(){
+window.initializeScale = (function() {
+    
+    return function(filterImagePreview, SCALE_STEP, INITIAL_SCALE, adjustScale, uploadResizeControls) {
+        
+        var sizeControlButton = uploadResizeControls.querySelectorAll('button');
+        var sizeControlsInput = uploadResizeControls.querySelector('input');
+        
+        sizeControlsInput.value = INITIAL_SCALE;
+        
+        var scale = function() {
+            return parseInt(sizeControlsInput.value);
+        };
+        
+        var zoomUp = function(adjustScale) {
+            if (scale() <= 100-SCALE_STEP) {
+                sizeControlsInput.value = scale() + SCALE_STEP + '%';
+            };
+            if (typeof adjustScale === 'function') {
+                adjustScale(scale);
+            };
+        };
+        
+        var zoomDown = function(adjustScale) {
+            if (scale() >= SCALE_STEP) {
+                sizeControlsInput.value = scale() - SCALE_STEP + '%';
+            };
+            if (typeof adjustScale === 'function') {
+                adjustScale(scale);
+            };
+        };
+        
+        adjustScale(scale);
+                
+        sizeControlButton[1].addEventListener('click', function() {
+            zoomUp(adjustScale);
+        });
+        
+        sizeControlButton[0].addEventListener('click', function() {
+            zoomDown(adjustScale);
+        });
 
-var filterImagePreview = document.querySelector('.filter-image-preview');
-var uploadResizeControls = document.querySelector('.upload-resize-controls');
-var sizeControlButton = uploadResizeControls.querySelectorAll('button');
-var sizeControlsInput = uploadResizeControls.querySelector('input');
-var SCALE_STEP = 25;
-var INITIAL_SCALE = 100;
-
-var scale = function() {
-    return parseInt(sizeControlsInput.value);
-};
-
-var adjustScale = function() {
-    filterImagePreview.style.transform = 'scale(' + scale() / 100 + ')';
-};
-
-var zoomUp = function(adjustScale) {
-    if (scale() <= 100-SCALE_STEP) {
-        sizeControlsInput.value = scale() + SCALE_STEP + '%';
     };
-    if (typeof adjustScale === 'function') {
-        adjustScale();
-    };
-};
-    
-var zoomDown = function(adjustScale) {
-    if (scale() >= SCALE_STEP) {
-        sizeControlsInput.value = scale() - SCALE_STEP + '%';
-    };
-    if (typeof adjustScale === 'function') {
-        adjustScale();
-    };
-};
-    
-return function() {
-    
-    scale();
-    
-    adjustScale();
-
-    sizeControlButton[1].addEventListener('click', function() {
-        zoomUp(adjustScale);
-    });
-    
-    sizeControlButton[0].addEventListener('click', function() {
-        zoomDown(adjustScale);
-    });
-    
-};
 
 })();
