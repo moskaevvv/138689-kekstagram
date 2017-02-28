10d0e3fc98d0ae95f2440ae0953df4eac4d69d2b
Moskaevvv <moskaevvv@mail.ru>
2017-02-28 21:07:40 +0300

    исправил ошибки ESlint

--- js/form.js
+++ js/form.js
@@ -5,105 +5,98 @@ var ESCAPE_KEY_CODE = 27;
 var uploadFileInput = document.querySelector('#upload-file');
 
 
+// 1. Открытие-закрытие формы редактирования изображения
+
+var uploadFormToggle = (function () {
 
 
-//1. Открытие-закрытие формы редактирования изображения
+  var uploadSelectImage = document.querySelector('#upload-select-image');
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  var uploadFormCancel = document.querySelector('.upload-form-cancel');
 
-var uploadFormToggle = (function() {
-    
+// 1.1 Показ
 
-var uploadSelectImage = document.querySelector('#upload-select-image');
-var uploadOverlay = document.querySelector('.upload-overlay');
-var uploadFormCancel = document.querySelector('.upload-form-cancel');
-
-//1.1 Показ
-
-var uploadSelectImageClose = function() {
+  var uploadSelectImageClose = function () {
     uploadSelectImage.classList.add('invisible');
-};
-var uploadOverlayOpen = function() {
+  };
+  var uploadOverlayOpen = function () {
     uploadOverlay.classList.remove('invisible');
-};
+  };
 
-uploadFileInput.addEventListener('change', uploadSelectImageClose);
-uploadFileInput.addEventListener('change', uploadOverlayOpen);
+  uploadFileInput.addEventListener('change', uploadSelectImageClose);
+  uploadFileInput.addEventListener('change', uploadOverlayOpen);
 
-//1.2 Закрытие
+// 1.2 Закрытие
 
-var uploadOverlayClose = function() {
+  var uploadOverlayClose = function () {
     uploadOverlay.classList.add('invisible');
-};
-var uploadSelectImageOpen = function() {
+  };
+  var uploadSelectImageOpen = function () {
     uploadSelectImage.classList.remove('invisible');
-};
+  };
 
-uploadFormCancel.addEventListener('click', uploadOverlayClose);
-uploadFormCancel.addEventListener('click', uploadSelectImageOpen);
+  uploadFormCancel.addEventListener('click', uploadOverlayClose);
+  uploadFormCancel.addEventListener('click', uploadSelectImageOpen);
 
-//1.3 +ARIA
+// 1.3 +ARIA
 
-var overlayClosePress = function(evt) {
+  var overlayClosePress = function (evt) {
     if (evt.keyCode === ENTER_KEY_CODE) {
-        uploadFormCancel.setAttribute('aria-pressed', 'true');  
-    };    
-};
+      uploadFormCancel.setAttribute('aria-pressed', 'true');
+    }
+  };
 
-uploadFormCancel.addEventListener('keydown', overlayClosePress);
-    
+  uploadFormCancel.addEventListener('keydown', overlayClosePress);
+
 })();
 
 
+// 2. Применения фильтров
 
-
-
-//2. Применения фильтров
-
-
-(function() {
-
-    var filterControlPanel = document.querySelector('.upload-filter-controls');
-    var filterControls = filterControlPanel.querySelectorAll('input');
-    var filterImagePreview = document.querySelector('.filter-image-preview');
-    var filterID;
-    
-    //callback
-    var filterSwitcher = function(filterID) {
-        filterImagePreview.classList.remove('filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
-        if (filterID.substr(7) !== 'filter-none') {
-            filterImagePreview.classList.add(filterID.substr(7));
-        };
-    };
-    
-    //2.1 Функция
-    window.initializeFilters.mouseMethod(filterControls, filterSwitcher);
-
-    //2.2 +ARIA
-    
-    var filterControlLabels = filterControlPanel.querySelectorAll('.upload-filter-label');
-    window.initializeFilters.enterMethod(filterControlLabels, filterControls, filterSwitcher);
-
-    
-})();
-
-
-
-
-//3. Масштабирование изображения
 
 (function () {
-    
-    var filterImagePreview = document.querySelector('.filter-image-preview');
-    var uploadResizeControls = document.querySelector('.upload-resize-controls');
-    var SCALE_STEP = 25;
-    var INITIAL_SCALE = 100;
-    
-    var adjustScale = function(scale) {
-        filterImagePreview.style.transform = 'scale(' + scale() / 100 + ')';
-    };
-    
-    uploadFileInput.addEventListener('change', function() {
-        initializeScale(filterImagePreview, SCALE_STEP, INITIAL_SCALE, adjustScale, uploadResizeControls);
-    });
-    
+
+  var filterControlPanel = document.querySelector('.upload-filter-controls');
+  var filterControls = filterControlPanel.querySelectorAll('input');
+  var filterImagePreview = document.querySelector('.filter-image-preview');
+  var filterID;
+
+    // callback
+  var filterSwitcher = function (filterID) {
+    filterImagePreview.classList.remove('filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    if (filterID.substr(7) !== 'filter-none') {
+      filterImagePreview.classList.add(filterID.substr(7));
+    }
+  };
+
+    // 2.1 Функция
+  window.initializeFilters.mouseMethod(filterControls, filterSwitcher);
+
+    // 2.2 +ARIA
+
+  var filterControlLabels = filterControlPanel.querySelectorAll('.upload-filter-label');
+  window.initializeFilters.enterMethod(filterControlLabels, filterControls, filterSwitcher);
+
+
+})();
+
+
+// 3. Масштабирование изображения
+
+(function () {
+
+  var filterImagePreview = document.querySelector('.filter-image-preview');
+  var uploadResizeControls = document.querySelector('.upload-resize-controls');
+  var SCALE_STEP = 25;
+  var INITIAL_SCALE = 100;
+
+  var adjustScale = function (scale) {
+    filterImagePreview.style.transform = 'scale(' + scale() / 100 + ')';
+  };
+
+  uploadFileInput.addEventListener('change', function () {
+    initializeScale(filterImagePreview, SCALE_STEP, INITIAL_SCALE, adjustScale, uploadResizeControls);
+  });
+
 })();
 
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,46 +1,46 @@
 'use strict';
 
-window.initializeScale = (function() {
-    
-    return function(filterImagePreview, SCALE_STEP, INITIAL_SCALE, adjustScale, uploadResizeControls) {
-        
-        var sizeControlButton = uploadResizeControls.querySelectorAll('button');
-        var sizeControlsInput = uploadResizeControls.querySelector('input');
-        
-        sizeControlsInput.value = INITIAL_SCALE;
-        
-        var scale = function() {
-            return parseInt(sizeControlsInput.value);
-        };
-        
-        var zoomUp = function(adjustScale) {
-            if (scale() <= 100-SCALE_STEP) {
-                sizeControlsInput.value = scale() + SCALE_STEP + '%';
-            };
-            if (typeof adjustScale === 'function') {
-                adjustScale(scale);
-            };
-        };
-        
-        var zoomDown = function(adjustScale) {
-            if (scale() > SCALE_STEP) {
-                sizeControlsInput.value = scale() - SCALE_STEP + '%';
-            };
-            if (typeof adjustScale === 'function') {
-                adjustScale(scale);
-            };
-        };
-        
-        adjustScale(scale);
-                
-        sizeControlButton[1].addEventListener('click', function() {
-            zoomUp(adjustScale);
-        });
-        
-        sizeControlButton[0].addEventListener('click', function() {
-            zoomDown(adjustScale);
-        });
+window.initializeScale = (function () {
 
+  return function (filterImagePreview, SCALE_STEP, INITIAL_SCALE, adjustScale, uploadResizeControls) {
+
+    var sizeControlButton = uploadResizeControls.querySelectorAll('button');
+    var sizeControlsInput = uploadResizeControls.querySelector('input');
+
+    sizeControlsInput.value = INITIAL_SCALE;
+
+    var scale = function () {
+      return parseInt(sizeControlsInput.value);
     };
+
+    var zoomUp = function (adjustScale) {
+      if (scale() <= 100 - SCALE_STEP) {
+        sizeControlsInput.value = scale() + SCALE_STEP + '%';
+      }
+      if (typeof adjustScale === 'function') {
+        adjustScale(scale);
+      }
+    };
+
+    var zoomDown = function (adjustScale) {
+      if (scale() > SCALE_STEP) {
+        sizeControlsInput.value = scale() - SCALE_STEP + '%';
+      }
+      if (typeof adjustScale === 'function') {
+        adjustScale(scale);
+      }
+    };
+
+    adjustScale(scale);
+
+    sizeControlButton[1].addEventListener('click', function () {
+      zoomUp(adjustScale);
+    });
+
+    sizeControlButton[0].addEventListener('click', function () {
+      zoomDown(adjustScale);
+    });
+
+  };
 
 })();
